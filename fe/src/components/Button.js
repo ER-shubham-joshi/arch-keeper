bc47b172cded6e8d921847468b8c62e19d37e079
Shubham Joshi <theengineershubham@gmail.com>
2024-03-28 20:55:13 +0530

    Button modified to take icon and text both

--- fe/src/components/Button.js
+++ fe/src/components/Button.js
@@ -5,7 +5,7 @@ import { hoverBorder } from '../styles'
 const Button = ({ icon, borderColor, type, focus, buttonText, className, onClick }) => {
     return (
         <button onClick={onClick} type={type} className={`bg-gray-900 text-white px-3 py-2 m-2 rounded-md border-gray-900 border-2 focus:outline-none focus:border-blue-300 ${hoverBorder[borderColor]} focus:${focus} ${className}`}>
-            {icon ? <FontAwesomeIcon icon={icon} /> : buttonText}
+            {icon && <FontAwesomeIcon icon={icon} />} {buttonText}
         </button>
     )
 }
